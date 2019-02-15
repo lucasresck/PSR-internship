72607b0173abd86c14568373505c80c0c2788456
Lucas Domingues <lucasresck@eedm.com>
2019-02-15 09:00:53 -0200

    Updated function and notebooks; added a new folder Logs

--- Functions/comparison.jl
+++ Functions/comparison.jl
@@ -1,7 +1,7 @@
 using Plots, StatPlots, DataFrames
 
 function comparison(d, G, dados1, schedule_matrix1, dados2, schedule_matrix2; xlabel1 = "dados1", xlabel2 = "dados2",
-        title = "Comparação da capacidade disponível", legend1 = false, expected_value_of_demand = true)
+        title = "Comparação da capacidade disponível", legend1 = false, expected_value_of_demand = true, y_max1 = 350)
     nplants = length(G)
     nmonths = size(dados1, 1)
     if length(size(d)) == 1
@@ -12,11 +12,11 @@ function comparison(d, G, dados1, schedule_matrix1, dados2, schedule_matrix2; xl
     if expected_value_of_demand
         plot1 = groupedbar([dados1.capacity_available dados2.capacity_available [maximum(d[month, :]) for month in 1:nmonths] [sum(d[month, :]) / Ns for month in 1:nmonths]],
         label = [xlabel1; xlabel2; ["Maior demanda"]; ["Valor esp. da demanda"]],
-        xlabel = "Mês", ylabel = "Capacidade", xaxis = collect(1:nmonths), title = title, legend = legend1, ylims = (0, 350))
+        xlabel = "Mês", ylabel = "Capacidade", xaxis = collect(1:nmonths), title = title, legend = legend1, ylims = (0, y_max1))
     else
         plot1 = groupedbar([dados1.capacity_available dados2.capacity_available [maximum(d[month, :]) for month in 1:nmonths]],
         label = [xlabel1; xlabel2; ["Maior demanda"]],
-        xlabel = "Mês", ylabel = "Capacidade", xaxis = collect(1:nmonths), title = title, legend = legend1, ylims = (0, 350))
+        xlabel = "Mês", ylabel = "Capacidade", xaxis = collect(1:nmonths), title = title, legend = legend1, ylims = (0, y_max1))
     end
     plot2 = bar(collect(1:nmonths), [size(dados1.plants_off[month])[1] for month in 1:nmonths], xaxis = collect(1:nmonths),
         ylabel = "Nº de usinas desligadas", xlabel = xlabel1, legend = false)
